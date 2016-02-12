918bd189de7aa3f883e03c06eef4229014bb64fc
angien77 <angela.nguyen77@gmail.com>
2016-02-12 03:24:15 +0000

    updated lab02

--- lab02-Cyclometer/Cyclometer.java
+++ lab02-Cyclometer/Cyclometer.java
@@ -19,6 +19,7 @@ public class Cyclometer {
         int trip1Rotation = 1561;    // declares integer variable assigning the number of rotations for Trip 1
         int trip2Sec = 3220;         // declares integer varaible assigning the number of seconds for Trip2
         int trip2Rotation = 9037;    // declares integer variable assiging the number of rotations for Trip2
+        
         // Other information needed for calculations
         double diameterWheel = 27.0; // declares double variable assigning the diamter of the wheel
         double PI = 3.14159;                // assigns the value of pi to the variable PI
@@ -26,17 +27,20 @@ public class Cyclometer {
         double inchesToFeet = 12;           // assigns the conversion of 12 inches in 1 foot to variable
         double secondsToMinutes = 60;       // assigns the conversion of 60 seconds in 1 minutes to variable
         double trip1Distance, trip2Distance, totalDistance; // declares double variables for the distances of the trips and total distance
+        
         // Printing out the information of each trip
         System.out.println ("The time for Trip 1 was " + (trip1Sec / secondsToMinutes) + // Print out the information for Trip 1
         " minutes and had " + (trip1Rotation) + " counts.");
         System.out.println ("The time for Trip 2 was " + (trip2Sec / secondsToMinutes) + // Print out the information for Trip 2
         " minutes and had " + (trip2Rotation) + " counts.");
+        
         // Perform the calculations for distance traveled in miles and the total distance in mmiles
         trip1Distance = (trip1Rotation * diameterWheel * PI);       // gives the distance traveled in inches
         trip1Distance = trip1Distance / inchesToFeet / feetToMile;  // gives the distance traveled in miles 
         trip2Distance = (trip2Rotation * diameterWheel * PI);       // gives the distance traveled in inches
         trip2Distance = trip2Distance / inchesToFeet / feetToMile;  // gives the distance traveled in miles 
         totalDistance = trip1Distance + trip2Distance;                  // adds the two trip distances together for total distance traveled
+        
         // Print out the distances traveled for each trip in miles
         System.out.println ("The distance traveled for Trip 1 was " + (trip1Distance) + " miles."); // Print out the distance traveled for Trip 1 in miles
         System.out.println ("The distance traveled for Trip 2 was " + (trip2Distance) + " miles."); // Print out the distance traveled for Trip 2 in miles
