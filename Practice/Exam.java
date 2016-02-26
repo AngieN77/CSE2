5a852bc662be5184ca12b481f8a8198d97fbb0e5
angien77 <angela.nguyen77@gmail.com>
2016-02-26 18:22:40 +0000

    test

---
+++ Practice/Exam.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exam {
+    public static void  main (String[] args) {
+            Scanner scan = new Scanner( System.in );
+          System.out.println("input octal integer with value between 0 and 512.");
+
+          int octalVal = scan.nextInt();
+          
+          // it is also acceptable to have >=1000
+          if( octalVal < 0 || octalVal > 777 ){	
+               System.out.println("bad value.  exiting.\n");
+               System.exit(0); //not necessary
+          }
+          else{
+               int sixtyFoursDigit = octalVal / 100;
+               System.out.println (sixtyFoursDigit);
+               int eightsDigit = (octalVal % 100) / 10;
+               int onesDigit = (octalVal % 10);
+          
+               int decimalResult = (sixtyFoursDigit*64) + (eightsDigit*8) + (onesDigit*1);
+               System.out.println("The value is " + decimalResult + " in decimal.");
+          }
+
+    }
+}
