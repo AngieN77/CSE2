858f2069ba1f9b9ba0cf84239940ca46fbfed0e4
angien77 <angela.nguyen77@gmail.com>
2016-03-06 22:14:09 +0000

    finished TextSpinner - do not touch

--- hw06/TextSpinner.java
+++ hw06/TextSpinner.java
@@ -7,15 +7,15 @@
 
 public class TextSpinner {
     public static void main (String[] args) {
-        while (true) {
-            System.out.print ("/");
-            System.out.print ("\b");
-            System.out.print ("-");
-            System.out.print ("\b");
-            System.out.print ("\\");
-            System.out.print ("\b");
-            System.out.print ("|");
-            System.out.print ("\b");
+        while (true) {                              // Infinite while loop since condition is always set to true
+            System.out.print ("/");                 // Print out first stage of spin
+            System.out.print ("\b");                // Backspaces/erases first stage
+            System.out.print ("-");                 // Print out second stage of spin
+            System.out.print ("\b");                // Backspaces/erases second stage
+            System.out.print ("\\");                // Print out third stage of spin
+            System.out.print ("\b");                // Backspaces/erases third stage
+            System.out.print ("|");                 // Print out fourth stage of spin
+            System.out.print ("\b");                // Backspaces/erases fourth stage and loops back to the beginning
         }
     }
 }
